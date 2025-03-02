c0788bf1d684baa5f9d5463903ab416c75647ff1
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-02 21:28:46 +0100

    add: changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
 import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [post, setPost] = useState([])
+  const [newPost, setNewPost] = useState ('');
+
+  const handleNewPost = (e) => {
+    setNewPost(e.target.value)
+  }
 
   return (
     <>
+      <div className="container">
+        <h2>
+          Aggiungi post
+
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            value={newPost}
+            onChange={handleNewPost}
+            placeholder="Inserisci un post"
+          />
+
+          <button className="btn btn-primary">Aggiungi post</button>
+
+
+        </form>
+
+        <h2>Lista Posts</h2>
+        {
+          post.length > 0 ? (
+            <ul>
+              {
+                post.map( (element, index) => {
+                  return (
+                    <li key={index}>
+                      {element}
+                    </li>
+                  )
+
+                })
+              }
+            </ul>
+          ) : (
+            <h3>Non ci sono posts</h3>
+          )
+        }
+        
+      </div>
+
       
     </>
   )
